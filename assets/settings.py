c64c9f2f00bb764b53533f3a7fbe42a8c37655dc
unknown <mashoodurrehmanofficial@gmail.com>
2020-12-08 10:33:19 +0500

    ee2

--- assets/settings.py
+++ assets/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'or+(mb%)t+^83wy%+rgx#rg777k4&!z^08#=(@a*s5&*+#2!c1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
